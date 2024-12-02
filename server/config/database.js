8e1838f1cf93791c793eda9a964de7fc475325b5
kyibell <148383705+kyibell@users.noreply.github.com>
2024-12-02 15:02:44 -0600

    Added bcrypt for password hashing

--- server/config/database.js
+++ server/config/database.js
@@ -6,19 +6,19 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config({path: `${__dirname}/../../.env`});
+dotenv.config({path: `${__dirname}/../../.env`}); // Had to find the env file like this because wasn't detecting the file
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, { // Create a new Sequelize instance for database config
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    logging: false,
+    host: process.env.DB_HOST, // Host parameter
+    port: process.env.DB_PORT, // Port Parameter
+    logging: false, // Turn off logging for errors
     dialectOptions: {
         ssl: {
-            require: true,
-            rejectUnauthorized: false,
+            require: true, // SSL required for Supabase
+            rejectUnauthorized: false, 
         },
     },
 });
 
-export default sequelize;
+export default sequelize; // Export object
